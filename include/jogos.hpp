e45888f59443822d4c82f4a3fb02414a58105ca8
raizaw <raizawunsch@gmail.com>
2025-01-18 14:17:02 -0300

    [tabuleiro.cpp] / [jogos.hpp] modificações

--- include/jogos.hpp
+++ include/jogos.hpp
@@ -3,20 +3,33 @@
 
 #include "tabuleiro.hpp"
 #include <iostream>
+#include <vector>
 
 // Classe abstrata
 class Jogos {
 protected:
-    Tabuleiro tabuleiro;
+    Tabuleiro* tabuleiro;
 
 public:
-    Jogos(int linhas, int colunas);
+    Jogos(int linhas, int colunas) {
+        tabuleiro = new Tabuleiro(linhas, colunas); // Inicializa o tabuleiro
+    }
 
+    // Métodos para imprimir o tabuleiro
+    virtual void exibirTabuleiro() const;
+    virtual void exibirPeca() const = 0;
     
+    virtual void lerJogada() const = 0;
+	virtual bool jogadaEValida();
+	virtual void realizarJogada();
+	virtual bool partidaAcabou();
+	virtual void indicarFimDaPartida();
+	virtual void trocarJogador();
 
 
-
-
+    virtual ~Jogos() {
+        delete tabuleiro; // Libera a memória do tabuleiro
+    }
 };
 
 #endif
--- src/tabuleiro.cpp
+++ src/tabuleiro.cpp
@@ -1,4 +1,36 @@
 #include "tabuleiro.hpp"
 
-Tabuleiro::Tabuleiro(int linhas, int colunas) 
-    : linhas(linhas), colunas(colunas), tabuleiro(linhas, std::vector<int>(colunas, static_cast<int>(Peca::VAZIO))) {}
+Tabuleiro::Tabuleiro(int linhas, int colunas)
+    : linhas(linhas), colunas(colunas), tabuleiro(linhas, std::vector<Peca>(colunas, Peca::VAZIO)) {}
+
+// void Tabuleiro::imprimirTabuleiro() const {
+//     for (const auto& linha : tabuleiro) {
+//         for (const auto& celula : linha) {
+//             switch (celula) {
+//             case Peca::VAZIO:
+//                 std::cout << "  ";
+//                 break;
+//             case Peca::JOGADOR1:
+//                 std::cout << "X ";
+//                 break;
+//             case Peca::JOGADOR2:
+//                 std::cout << "O ";
+//                 break;
+//             }
+//         }
+//         std::cout << "\n";
+//     }
+// }
+
+void Tabuleiro::setPosicao(int linha, int coluna, Peca peca) {
+    if (linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas) {
+        tabuleiro[linha][coluna] = peca;
+    }
+}
+
+Tabuleiro::Peca Tabuleiro::getPosicao(int linha, int coluna) const {
+    if (linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas) {
+        return tabuleiro[linha][coluna];
+    }
+    return Peca::VAZIO;
+}
