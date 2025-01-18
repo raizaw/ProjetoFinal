0277e9bc4d41c88e24df3020ba5a08d696749fdc
raizaw <raizawunsch@gmail.com>
2025-01-18 15:49:46 -0300

    [jogoDaVelha.hpp] início da implementação do jogo da velha

---
+++ include/jogoDaVelha.hpp
@@ -0,0 +1,27 @@
+#ifndef JOGODAVELHA_H
+#define JOGODAVELHA_H
+
+#include "jogos.hpp"
+#include <iostream>
+
+class JogoDaVelha : public Jogos {
+public:
+    JogoDaVelha() : Jogos(3, 3) {} // Inicializa o tabuleiro com as dimensões apropriadas
+    ~JogoDaVelha() {};
+
+    // Métodos para imprimir o tabuleiro
+    // virtual void exibirTabuleiro() const override; -> declarar apenas se for sobrescrever
+    virtual void exibirPeca(Tabuleiro::Peca peca) const override; 
+    
+    // Métodos responsáveis pelas jogadas em cada rodada
+    virtual void lerJogada() const override; 
+	virtual bool jogadaEValida() const override; 
+	virtual void realizarJogada() override; 
+
+    // Métodos que gerenciam o andamento da partida
+	virtual bool partidaAcabou() const override; 
+	virtual void indicarFimDaPartida() const override;
+
+};
+
+#endif
