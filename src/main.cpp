a08bcd23e8ce4b7d9640b6defd0774d9785fba9f
raizaw <raizawunsch@gmail.com>
2025-01-25 01:05:48 -0300

    [main.cpp] inicio

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "../include/gestaoDeJogadores.hpp"
+#include "../include/lig4.hpp"
+#include "../include/jogoDaVelha.hpp"
+#include "../include/reversi.hpp"
+
+#include <iostream>
+
+int main(){
+    const std::string caminhodoArquivo = "dados.csv";
+    GestaoDeJogadores gestao(caminhodoArquivo);
+}
